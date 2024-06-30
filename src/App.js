80647e38ee0c5162ae481e607b984d6743b666c3
Min Khant Kyaw <mkhantk102@gmail.com>
2024-06-30 14:51:45 +0630

    Add basic html layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render(){
+    return(
+      <div className='flex justify-center items-center min-h-screen'>
+        <div className='container flex flex-col justify-center items-center'>
+          <div id='heading' className='flex flex-col items-center'>
+            <h1>25+5 clock</h1>
+            <p>Pomodoro Productivity Clock</p>
+          </div>
+          <div id='adjustment' className='flex gap-4 flex-wrap'>
+            <div className='flex flex-col items-center'>
+              <label>Break Length</label>
+              <div className='flex gap-2'>
+                <button>up</button>
+                <di>display</di>
+                <button>down</button>
+              </div>
+            </div>
+            <div className='flex flex-col items-center'>
+              <label>Session Length</label>
+              <div className='flex gap-2'>
+                <button>up</button>
+                <di>display</di>
+                <button>down</button>
+              </div>
+            </div>
+          </div>
+          <div id='display' className='w-[300px] h-[150px] ring-black ring-4 rounded'>25:00</div>
+          <div id='buttons' className='flex gap-2'>
+            <button><i className="ri-play-circle-line"></i></button>
+            <button>pause</button>
+            <button>reset</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
+
 
 export default App;
